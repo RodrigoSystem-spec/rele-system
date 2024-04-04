646d6773f7a9cfb350efca813ffcfb8c1d65a049
Jason Rodd <76754786+RodrigoSystem-spec@users.noreply.github.com>
2024-04-04 20:21:48 -0300

    Add files via upload
    Este bash a través de su ejecución con lets-rock.py se encarga de capturar el video de una cámara IP y guardarlo en un archivo MP4 con un nombre único que incluye la fecha, la hora y el día de la semana en el que se realizó la captura.

---
+++ stream.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+dia=$(date +%A)
+case $dia in
+  Monday)
+     diaact=Lunes
+  ;;
+  Tuesday)
+     diaact=Martes
+  ;;
+  Wednesday)
+     diaact=Miercoles
+  ;;
+  Thursday)
+     diaact=Jueves
+  ;;
+  Friday)
+     diaact=Viernes
+  ;;
+  Saturday)
+     diaact=Sabado
+  ;;
+  Sunday)
+    diaact=Domingo
+  ;;
+esac
+
+ffmpeg -v quiet -rtsp_transport tcp -i "rtsp://admin:Transito@192.168.1.108:554/cam/realmonitor?channel=1&subtype=0" -c copy -an "/home/pi/R-System/Videos/$diaact/video_$(date +"%Y%m%d%H%M%S").mp4"
